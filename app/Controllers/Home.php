d128de3bda325e32d487a3fdbb1da4b7c421a15b
schwgon <gasparschwartz@alumnos.itr3.edu.ar>
2024-08-02 13:08:26 -0300

    02-08-2024 guardar datos de dispositivo en sesion. En proceso

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -9,12 +9,12 @@ class Home extends BaseController
     public function index() // Metodo para cargar la pagina principal
     {
         $deviceModel = new DeviceModel();
-
         $id_usuario = $this->session->get('user_id');
-        
-        $resultado['dispositivo'] = $deviceModel->Dispositivo($id_usuario);
+        $resultado = $deviceModel->Dispositivo($id_usuario);
 
-        echo view('common/header', $resultado, ['session' => $this->session]); // Carga y muestra la vista 'common/header' pasando los datos de la sesion a la misma para su utilizacion.
+        $this->session->set('dispositivos', $resultado);  // Guarda los datos en la sesión
+
+        echo view('common/header', ['session' => $this->session]); // Carga y muestra la vista 'common/header' pasando los datos de la sesion a la misma para su utilizacion.
         echo view('common/footer', ['session' => $this->session]);
         return view('inicio'); // Carga y muestra la vista 'inicio'.
     }
--- app/Views/common/header.php
+++ app/Views/common/header.php
@@ -38,7 +38,13 @@ $session = \Config\Services::session();
                                     <div id="dropdown-menu" class="hidden absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
                                         <div class="py-1" role="none">
                                             <a href="<?= site_url('device'); ?>" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="menu-item-1">Add Device</a>
-                                            <?php foreach ($dispositivo as $dispo): ?>
+                                            <!-- <?//php foreach ($dispositivo as $dispo): ?>
+                                                <a href="<?//= site_url('mostrar_datos'); ?>" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="menu-item-<?//= htmlspecialchars($dispo['id_dispositivo']); ?>">
+                                                    <?//php echo htmlspecialchars($dispo['nombre']); ?>
+                                                </a>
+                                            <?//php endforeach; ?> -->
+                                            <?php $dispositivos = $session->get('dispositivo') ?? [];
+                                                foreach ($dispositivos as $dispo): ?>
                                                 <a href="<?= site_url('mostrar_datos'); ?>" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="menu-item-<?= htmlspecialchars($dispo['id_dispositivo']); ?>">
                                                     <?php echo htmlspecialchars($dispo['nombre']); ?>
                                                 </a>
