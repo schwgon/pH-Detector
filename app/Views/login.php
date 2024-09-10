54e57780b6c26eb31ba497c04f5b57aaa39d9652
lukeneitor <lucasfernandez@alumnos.itr3.edu.ar>
2024-09-10 08:09:21 -0300

    10-9

--- app/Views/login.php
+++ app/Views/login.php
@@ -4,36 +4,46 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dark Mode Login</title>
+    <title>Login</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="<?php echo base_url('css/styles.css'); ?>">
 </head>
 
-<body class="bg-white flex justify-center items-center min-h-screen">
-    <div class=" mt-8 bg-gray-100 p-8 shadow-sm max-w-md w-full mx-auto my-auto border-2 border-black">
-        <h1 class="text-black text-2xl font-sans mb-6 text-center">Log in</h1>
-            <?php if (session()->getFlashdata('error_message')): ?>
-                <div class="alert alert-error text-center mb-4 text-gray-300">
-                    <?= session()->getFlashdata('error_message') ?>
-                </div>
-            <?php endif; ?>
+<body class="bg-gray-100 dark:bg-gray-800 flex justify-center items-center min-h-screen">
+    <div class="dark:text-black p-8 shadow-md max-w-md w-full mx-auto border-2 border-emerald-400">
+        <h1 class="text-2xl font-sans mb-6 text-center">Log In</h1>
+        <?php if (session()->getFlashdata('error_message')): ?>
+            <div class="alert alert-error text-center mb-4 text-gray-300">
+                <?= session()->getFlashdata('error_message') ?>
+            </div>
+        <?php endif; ?>
         <form method="post" action="<?= base_url("loginForm"); ?>">
             <div class="mb-4">
-                <label for="email" class="block text-black mb-2 font-sans"><i class="fas fa-envelope"></i> Email</label>
-                <input type="text" class="form-input w-full bg-gray-200 text-black pl-2" id="email" name="email" placeholder="Email">
+                <label for="email" class="block  mb-2 font-sans"><i class="fas fa-envelope"></i> Email</label>
+                <input type="text" class="form-input w-full bg-gray-200 text-black pl-2" id="email" name="email"
+                    placeholder="Email">
             </div>
             <div class="mb-6">
-                <label for="password" class="block text-black mb-2 font-sans"><i class="fas fa-lock"></i> Password</label>
-                <input type="password" class="form-input w-full bg-gray-200 text-black pl-2" id="password" name="password" minlength="8" placeholder="Password">
+                <label for="password" class="block  mb-2 font-sans"><i class="fas fa-lock"></i> Password</label>
+                <input type="password" class="form-input w-full bg-gray-200 text-black pl-2" id="password"
+                    name="password" minlength="8" placeholder="Password">
             </div>
-            <button type="submit" class="bg-gray-100 hover:bg-gray-200 text-black font-sans py-2 px-4 w-full mb-4">Log in <i class="fas fa-chevron-right"></i></button>
+            <button type="submit"
+                class="bg-emerald-400 hover:bg-emerald-500 text-white font-sans py-2 w-full px-4 mb-4">
+                Log In <i class="fas fa-chevron-right"></i>
+            </button>
         </form>
-        <div class="text-black text-center">
-            <p>You do not have an account?<a href="<?= site_url("register"); ?>" class="text-black hover:text-gray-700 ml-2">Sign Up</a></p>
+        <p class="font-light text-center">
+            You don't have an account
+            <a href="<?= site_url("register"); ?>" class="text-emerald-400 hover:text-emerald-500 ml-2">
+                Register
+            </a>
+        </p>
+        <div class="text-center">
+            <p><a href="<?= site_url("restore_password"); ?>" class="hover:text-gray-200 ml-2">¿Olvido su
+                    contraseña?</a></p>
         </div>
-        <div class="text-black text-center">
-        <p><a href="<?= site_url("restore_password"); ?>" class="text-black hover:text-gray-700 ml-2">¿Olvido su contraseña?</a></p>
-        </div>
+
     </div>
 
 </body>
--- app/Views/register.php
+++ app/Views/register.php
@@ -12,7 +12,7 @@
 <body class="bg-gray-100 dark:bg-gray-800 flex justify-center items-center min-h-screen">
 
     <!-- Tarjeta del formulario con fondo blanco en modo claro y fondo negro en modo oscuro -->
-    <div class="dark:text-black p-8 shadow-md max-w-md w-full mx-auto border-2 border-emerald-400 rounded-lg">
+    <div class="dark:text-black p-8 shadow-md max-w-md w-full mx-auto border-2 border-emerald-400 ">
         <h1 class="text-2xl font-sans mb-6 text-center">Register</h1>
 
         <?php if (session()->getFlashdata('error_message')): ?>
@@ -25,19 +25,19 @@
             <div class="mb-4">
                 <label for="name" class="block mb-2 font-sans"><i class="fas fa-user"></i> User</label>
                 <input name="name" required type="text" pattern="[A-Za-zÀ-ÿ\u00f1\u00d1\s]+"
-                    class="form-input w-full bg-gray-200 dark:bg-gray-700 text-black dark:text-white font-light pl-2"
+                    class="form-input w-full bg-gray-200 dark:bg-gray-900 text-black dark:text-white font-light pl-2"
                     id="name" placeholder="Your Name">
             </div>
             <div class="mb-4">
                 <label for="email" class="block mb-2 font-sans"><i class="fas fa-envelope font-sans"></i> Email</label>
                 <input name="email" required type="email"
-                    class="form-input w-full bg-gray-200 dark:bg-gray-700 text-black dark:text-white font-light pl-2"
+                    class="form-input w-full bg-gray-200 dark:bg-gray-900 text-black dark:text-white font-light pl-2"
                     id="email" placeholder="example@mail.com">
             </div>
             <div class="mb-6">
                 <label for="password" class="block mb-2 font-sans"><i class="fas fa-lock"></i> Password</label>
                 <input name="password" required type="password"
-                    class="form-input w-full bg-gray-200 dark:bg-gray-700 text-black dark:text-white font-light pl-2"
+                    class="form-input w-full bg-gray-200 dark:bg-gray-900 text-black dark:text-white font-light pl-2"
                     id="password" minlength="8" placeholder="At least 8 characters">
             </div>
             <button type="submit"
