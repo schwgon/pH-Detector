b6e33a7224766fce081cfe91a38cd9f9f7b37403
Lukeneitor <lucasfernandez@alumnos.itr3.edu.ar>
2024-09-19 02:24:59 -0300

    19/09 cambios en la pagina UX/UI (Footer y Register)

--- app/Views/register.php
+++ app/Views/register.php
@@ -1,19 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro de Usuario</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="<?php echo base_url('css/register.css'); ?>">
     <link rel="stylesheet" href="<?php echo base_url('css/styles.css'); ?>">
 </head>
+<body class="flex justify-center items-center min-h-screen">
 
-<body class="bg-gray-100 dark:bg-gray-800 flex justify-center items-center min-h-screen">
-
-    <!-- Tarjeta del formulario con fondo blanco en modo claro y fondo negro en modo oscuro -->
-    <div class=" p-8 shadow-md max-w-md w-full mx-auto border-2 border-emerald-400 ">
-        <h1 class="text-2xl font-sans mb-6 text-center">Register</h1>
+    <!-- Tarjeta del formulario con fondo claro/oscuro -->
+    <div class="card p-8 shadow-md max-w-5xl w-full mx-auto border-2 border-emerald-400">
+        <h1 class="signup">Register</h1>
 
         <?php if (session()->getFlashdata('error_message')): ?>
             <div class="alert alert-correo text-center mb-4 text-red-600">
@@ -22,37 +21,26 @@
         <?php endif; ?>
 
         <form method="post" action="<?= base_url("register1"); ?>">
-            <div class="mb-4">
-                <label for="name" class="block mb-2 font-sans"><i class="fas fa-user"></i> User</label>
-                <input name="name" required type="text" pattern="[A-Za-zÀ-ÿ\u00f1\u00d1\s]+"
-                    class="form-input w-full font-light pl-2"
-                    id="name" placeholder="Your Name">
+            <div class="inputBox">
+                <input name="name" required type="text" pattern="[A-Za-zÀ-ÿ\u00f1\u00d1\s]+" placeholder=" " />
+                <span>User</span>
             </div>
-            <div class="mb-4">
-                <label for="email" class="block mb-2 font-sans"><i class="fas fa-envelope font-sans"></i> Email</label>
-                <input name="email" required type="email"
-                    class="form-input w-full font-light pl-2"
-                    id="email" placeholder="example@mail.com">
+            <div class="inputBox">
+                <input name="email" required type="email" placeholder="" />
+                <span>Email</span>
             </div>
-            <div class="mb-6">
-                <label for="password" class="block mb-2 font-sans"><i class="fas fa-lock"></i> Password</label>
-                <input name="password" required type="password"
-                    class="form-input w-full font-light pl-2"
-                    id="password" minlength="8" placeholder="At least 8 characters">
+            <div class="inputBox">
+                <input name="password" required type="password" minlength="8" placeholder=" " />
+                <span>Password</span>
             </div>
-            <button type="submit"
-                class="bg-emerald-400 hover:bg-emerald-500 text-white font-sans py-2 w-full px-4 mb-4">
-                Register <i class="fas fa-chevron-right"></i>
-            </button>
-            <p class="font-light text-center">
-                Do you already have an account?
-                <a href="<?= site_url("login"); ?>" class="text-emerald-400 hover:text-emerald-500 ml-2">
-                    Log-In
-                </a>
-            </p>
+            <button type="submit" class="enter">Enter</button>
         </form>
+
+        <span class="text-center mt-4">
+            Do you already have an account?
+            <a href="<?= site_url("login"); ?>" class="text-emerald-400 hover:text-emerald-500 ml-2">Log In</a>
+        </span>
     </div>
 
 </body>
-
 </html>
